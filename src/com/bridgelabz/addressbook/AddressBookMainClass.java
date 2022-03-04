2a1494d1037d9cc399d11e9809a965965f492a33
Sachin2022 <sachikatarki1995@gmail.com>
2022-03-04 10:46:23 +0530

    Added:Code to View Person in City or State across in Multiple Address Book

--- src/com/bridgelabz/addressbook/AddressBookMainClass.java
+++ src/com/bridgelabz/addressbook/AddressBookMainClass.java
@@ -26,7 +26,7 @@ public class AddressBookMainClass {
 
 		System.out.println("Enter your choice");
 		System.out.println(
-				"1 : Add new contact    2 : Edit contact  3 : Delete contact  4: Add Multiple Contacts 5: Display Contacts 6: Search Person");
+				"1 : Add new contact    2 : Edit contact  3 : Delete contact  4: Add Multiple Contacts 5: Display Contacts 6: Search Person 7: Person with City and State");
 		int choice = sc.nextInt();
 		switch (choice) {
 		case 1:
@@ -126,6 +126,10 @@ public class AddressBookMainClass {
 			System.out.println("Please Enter the State Name ");
 			String statename = sc.next();
 			addressbooks.searchPerson(cityname, statename);
+			addressbooks.addContacts();
+			break;
+		case 7:
+			viewCityAndPersonAsWellAsStateAndPesron();
 			addressbooks.addContacts();
 			break;
 		default:
@@ -307,6 +311,20 @@ public class AddressBookMainClass {
 				System.out.println("This peson not present in this city or state");
 			}
 
+		}
+	}
+
+	public void viewCityAndPersonAsWellAsStateAndPesron() {
+		List<Contact> contactsList = new ArrayList<>();
+		for (Map.Entry<String, AddressBook> set : addressBookSystem.entrySet()) {
+			AddressBook addressBook = set.getValue();
+			contactsList = addressBook.getContacts();
+			System.out.println("Person Name and His/her city");
+			contactsList.stream()
+					.forEachOrdered(con -> System.out.println(con.getFirstName() + "     " + con.getCity()));
+			System.out.println("Person Name and His/her State");
+			contactsList.stream()
+					.forEachOrdered(con -> System.out.println(con.getFirstName() + "     " + con.getState()));
 		}
 
 	}
